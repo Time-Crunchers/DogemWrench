abd25979242e855f400c62195970933a258f00a8
AltonRune <2of7.burton@gmail.com>
2019-05-08 13:25:24 -0400

    The panRegester

--- DogemWrench/DogemWrench/GameViewController.swift
+++ DogemWrench/DogemWrench/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
@@ -30,6 +31,15 @@ class GameViewController: UIViewController {
             view.showsFPS = false
             view.showsNodeCount = false
         }
+    }
+    
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
     
     override var shouldAutorotate: Bool {
