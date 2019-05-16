83c6bff932862de198b9a3d3d256021f1147dd24
Todd Wesley <hpro1@sv156-imac23.sylvaniaschools.local>
2019-05-16 14:19:21 -0400

    Clean Up

--- DogemWrench/DogemWrench/GameScene.swift
+++ DogemWrench/DogemWrench/GameScene.swift
@@ -1,17 +1,8 @@
-/* TODO list
- Add touch to start on main top
- Kill and breed launcher children
- Detect screen to place items
- create table method
- clean up
- finish building and sizing table
- 
- REQUEST LIST
- Joint paddles
- Detect Touch
- Kill And Breed Children
- 
- */
+
+
+// "If I take on the job, I complete it. And everyone in the galexy knows that"
+
+
 
 //
 //  GameScene.swift
@@ -49,13 +40,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var rNumber: Int = 2
     var ballNumber: Int = 0 {
         didSet {
-           // ballLabel.text = "Ball: \(ballNumber)"
+           
             
         }
     }
     var score: Int = 0 {
         didSet {
-           // scoreLabel.text = "Score: \(score)"
+          
             rNumber = Int(arc4random_uniform(5))
             print(rNumber)
         }
@@ -92,28 +83,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
         
-        
-        
-//        //
-//        //   Create Score
-//        //
-//        //
-//        scoreLabel = SKLabelNode(fontNamed: "DINCondensed-Bold ")
-//        scoreLabel.text = "Score: 0"
-//        scoreLabel.horizontalAlignmentMode = .right
-//        scoreLabel.position = CGPoint(x: 430, y: 900)
-//        addChild(scoreLabel)
-//
-//        //
-//        //   Create Ball Count
-//        //
-//        //
-//        ballLabel = SKLabelNode(fontNamed: "DINCondensed-Bold ")
-//        ballLabel.text = "Ball: 0"
-//        ballLabel.horizontalAlignmentMode = .right
-//        ballLabel.position = CGPoint(x: 425, y: 960)
-//        addChild(ballLabel)
-        
         //
         //   Create Start
         //
@@ -125,7 +94,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(startLabel)
         
         //
-        //   Flipper Buttons
+        //   Buttons
         //
         //
         rightLabel = SKLabelNode(fontNamed: "DINCondensed-Bold ")
@@ -151,10 +120,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
-    //
-    //  Deal with later
-    //
-    //
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         if let touch = touches.first {
@@ -216,9 +182,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 person.name = "person"
                 
                 addChild(person)
-                
-                
-                
                
             }
         }
@@ -231,26 +194,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //
     func makeSlot(at position: CGPoint, isGood: Bool) {
         var slotBase: SKSpriteNode
-        //var slotGlow: SKSpriteNode
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
-            // slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
             slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
-            // slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
             slotBase.name = "bad"
         }
         slotBase.position = position
-        //slotGlow.position = position
         slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
         slotBase.physicsBody!.isDynamic = false
         addChild(slotBase)
-        //  addChild(slotGlow)
-        
-        // let spin = SKAction.rotate(byAngle: CGFloat.pi, duration: 10)
-        // let spinForever = SKAction.repeatForever(spin)
-        // slotGlow.run(spinForever)
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
@@ -271,7 +225,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             destroy(ball: ball)
             
         } else if object.name == "bad" {
-            // score -= 1
             destroy(ball: ball)
             
         } else if object.name == "rightFlipper" {
@@ -304,13 +257,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.removeFromParent()
         person.removeFromParent()
         
-//        if (ballNumber < 3) {
-//            startLabel = SKLabelNode(fontNamed: "DINCondensed-Bold ")
-//            startLabel.text = "Press to Start"
-//            startLabel.horizontalAlignmentMode = .right
-//            startLabel.position = CGPoint(x: 475, y: 500)
-//            addChild(startLabel)
-//        } else {
             gameOverLabel = SKLabelNode(fontNamed: "DINCondensed-Bold ")
             gameOverLabel.text = "Game Over"
             gameOverLabel.horizontalAlignmentMode = .right
@@ -324,8 +270,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             ballNumber = 0
             score = 0
             gameOver = true
-            
-//        }
         
     }
     
@@ -348,7 +292,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         bouder.physicsBody!.isDynamic = false
         bouder.zPosition = 2
-        //  paddles.physicsBody!.restitution = 2
         addChild(bouder)
     }
     
@@ -386,195 +329,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func killLauncher(launcher: SKNode) {
         launcher.removeFromParent()
         print("I'm sorry for your loss")
-        
-    }
-    //
-    //  Slope Right
-    //
-    //
-    func makeSlopeRight (at position: CGPoint) {
-        let slopeRight = SKSpriteNode(imageNamed: "shape_cornerCurvedRight")
-        slopeRight.position = position
-        slopeRight.physicsBody = SKPhysicsBody(texture: slopeRight.texture!, size: slopeRight.texture!.size())
-        slopeRight.physicsBody!.isDynamic = false
-        // slopeRight.physicsBody!.restitution = 5000
-        //  launcher.name = "good"
-        addChild(slopeRight)
-    }
-    
-    //
-    //  Slope Left
-    //
-    //
-    func makeSlopeLeft (at position: CGPoint) {
-        let slopeLeft = SKSpriteNode(imageNamed: "shape_cornerCurvedLeft")
-        slopeLeft.position = position
-        slopeLeft.physicsBody = SKPhysicsBody(texture: slopeLeft.texture!, size: slopeLeft.texture!.size())
-        slopeLeft.physicsBody!.isDynamic = false
-        // slopeRight.physicsBody!.restitution = 5000
-        //  launcher.name = "good"
-        addChild(slopeLeft)
-    }
-    
-    //
-    //  Block
-    //
-    //
-    func makeRectangle (at position: CGPoint) {
-        let rectangle = SKSpriteNode(imageNamed: "shape_rectangle")
-        rectangle.position = position
-        rectangle.physicsBody = SKPhysicsBody(texture: rectangle.texture!, size: rectangle.texture!.size())
-        rectangle.physicsBody!.isDynamic = false
-        addChild(rectangle)
-    }
-    
-    //
-    //  Guard Left
-    //
-    //
-    func makeGuardLeft (at position: CGPoint) {
-        let guardLeft = SKSpriteNode(imageNamed: "paddleHolder_innerLeft")
-        guardLeft.position = position
-        guardLeft.physicsBody = SKPhysicsBody(texture: guardLeft.texture!, size: guardLeft.texture!.size())
-        guardLeft.physicsBody!.isDynamic = false
-        guardLeft.physicsBody!.restitution = 2
-        //  launcher.name = "good"
-        addChild(guardLeft)
-    }
-    
-    //
-    //  Guard Right
-    //
-    //
-    func makeGuardRight (at position: CGPoint) {
-        let guardRight = SKSpriteNode(imageNamed: "paddleHolder_innerRight")
-        guardRight.position = position
-        guardRight.physicsBody = SKPhysicsBody(texture: guardRight.texture!, size: guardRight.texture!.size())
-        guardRight.physicsBody!.isDynamic = false
-        guardRight.physicsBody!.restitution = 2
-        //  launcher.name = "good"
-        addChild(guardRight)
-    }
-    //
-    //  Outer Guard Left
-    //
-    //
-    func makeOuterGuardLeft (at position: CGPoint) {
-        let outerGuardLeft = SKSpriteNode(imageNamed: "paddleHolder_outerLeft")
-        outerGuardLeft.position = position
-        outerGuardLeft.physicsBody = SKPhysicsBody(texture: outerGuardLeft.texture!, size: outerGuardLeft.texture!.size())
-        outerGuardLeft.physicsBody!.isDynamic = false
-        // outerGuardRight.physicsBody!.restitution = 5
-        //  launcher.name = "good"
-        addChild(outerGuardLeft)
-    }
-    
-    //
-    //  Outer Guard Right
-    //
-    //
-    func makeOuterGuardRight (at position: CGPoint) {
-        let outerGuardRight = SKSpriteNode(imageNamed: "paddleHolder_outerRight")
-        outerGuardRight.position = position
-        outerGuardRight.physicsBody = SKPhysicsBody(texture: outerGuardRight.texture!, size: outerGuardRight.texture!.size())
-        outerGuardRight.physicsBody!.isDynamic = false
-        outerGuardRight.zPosition = 0
-        // outerGuardRight.physicsBody!.restitution = 5
-        //  launcher.name = "good"
-        addChild(outerGuardRight)
-    }
-    
-    //
-    //  Paddles
-    //
-    //
-    func rightFlipperCreate() {
-        
-        print("1")
-        
-        
-        let size = CGSize(width: 20, height: 20)
-        
-        rightPeg = SKSpriteNode(color: .red,
-                                size: size)
-        rightPeg.zPosition = -5
-        
-        rightFlipper = SKSpriteNode(imageNamed: "flipperShortRight")
-        
-        rightPeg.physicsBody = SKPhysicsBody(rectangleOf: size)
-        rightPeg.physicsBody?.isDynamic = false
-        rightPeg.position = CGPoint(x: 547, y: 125)
-        
-        rightFlipper.physicsBody = SKPhysicsBody(texture: rightFlipper.texture!, size: rightFlipper.texture!.size())
-        rightFlipper.position = CGPoint(x: 490, y: 125)
-        rightFlipper.physicsBody!.mass = 999999
-        
-        rightFlipper.name = "rightFlipper"
-        addChild(rightPeg)
-        addChild(rightFlipper)
-        
-        let rightConection = SKPhysicsJointFixed.joint(withBodyA: rightPeg.physicsBody!, bodyB: rightFlipper.physicsBody!, anchor: rightPeg.position)
-        
-        // let rightFlipper = SKPhysicsJointPin.joint(withBodyA: rightPeg.physicsBody!, bodyB: dynamicNode.physicsBody!, anchor: rightPeg.position)
-        
-        // let rightFlipper = SKPhysicsJointSpring.joint(withBodyA: rightPeg.physicsBody!, bodyB: dynamicNode.physicsBody!, anchorA: rightPeg.position, anchorB: dynamicNode.position)
-        
-        // rightFlipper.frequency = 0.5
-        // rightFlipper.damping = 0.2
-        
-        physicsWorld.add(rightConection)
-        
-        //        let move = SKAction.moveBy(x:0, y: 400,
-        //                                   duration: 5)
-        //        let spin = SKAction.rotate(byAngle: 0.3, duration: 0.3)
-        //        let hit = SKAction.repeat(spin, count: 3)
-        //        rightPeg.run(move)
-        print("2")
-        
-    }
-    
-    func leftFlipperCreate() {
-        
-        print("1")
-        
-        
-        let size = CGSize(width: 20, height: 20)
-        
-        leftPeg = SKSpriteNode(color: .red,
-                               size: size)
-        leftPeg.zPosition = -5
-        
-        leftFlipper = SKSpriteNode(imageNamed: "flipperShortLeft")
-        
-        leftPeg.physicsBody = SKPhysicsBody(rectangleOf: size)
-        leftPeg.physicsBody?.isDynamic = false
-        leftPeg.position = CGPoint(x: 143, y: 125)
-        
-        leftFlipper.physicsBody = SKPhysicsBody(texture: leftFlipper.texture!, size: leftFlipper.texture!.size())
-        leftFlipper.position = CGPoint(x: 200, y: 125)
-        leftFlipper.physicsBody!.mass = 999999
-        
-        leftFlipper.name = "leftFlipper"
-        addChild(leftPeg)
-        addChild(leftFlipper)
-        
-        let leftConection = SKPhysicsJointFixed.joint(withBodyA: leftPeg.physicsBody!, bodyB: leftFlipper.physicsBody!, anchor: leftPeg.position)
-        
-        // let rightFlipper = SKPhysicsJointPin.joint(withBodyA: rightPeg.physicsBody!, bodyB: dynamicNode.physicsBody!, anchor: rightPeg.position)
-        
-        // let rightFlipper = SKPhysicsJointSpring.joint(withBodyA: rightPeg.physicsBody!, bodyB: dynamicNode.physicsBody!, anchorA: rightPeg.position, anchorB: dynamicNode.position)
-        
-        // rightFlipper.frequency = 0.5
-        // rightFlipper.damping = 0.2
-        
-        physicsWorld.add(leftConection)
-        
-        //        let move = SKAction.moveBy(x:0, y: 400,
-        //                                   duration: 5)
-        //        let spin = SKAction.rotate(byAngle: 0.3, duration: 0.3)
-        //        let hit = SKAction.repeat(spin, count: 3)
-        //        rightPeg.run(move)
-        print("2")
         
     }
 }
